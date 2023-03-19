84117d7b73e250e5ec6f1edbd72d38836365925a
Artem Zhukov <107498838+Artemjzhukov@users.noreply.github.com>
2023-03-19 23:20:37 +0400

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,43 @@
+my_tuple  = 24, 42, 2.3456, "Hello"
+my_tuple
+
+(24, 42, 2.3456, 'Hello')
+
+my_tuple[1] = "New"
+
+my_tuple2 = "One", 
+my_tuple2
+
+('One',)
+
+my_tuple3 = ()
+my_tuple3
+
+()
+
+my_tuple = "hello", "there"
+my_tuple2 = my_tuple, 45, "guido"
+
+my_tuple
+
+('hello', 'there')
+
+my_tuple2
+
+(('hello', 'there'), 45, 'guido')
+
+my_tuple = "Hello", "World"
+hello, world = my_tuple
+print(hello)
+print(world)
+
+Hello
+World
+
+hello = "Welcome"
+print(hello)
+print(world)
+
+Welcome
+World
+
